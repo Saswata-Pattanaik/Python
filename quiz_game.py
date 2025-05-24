4e85b4a82cd747c6b57567f37d10bb522e6f4c6e
Saswata Pattanaik <saswatap.ai09@gmail.com>
2025-05-24 10:22:48 +0530

    recreated the whole program using oops

--- quiz_game.py
+++ quiz_game.py
@@ -1,42 +1,53 @@
 import time
 import random
-print("welcome to science quiz")
 
-player = input("do u want to begin playing ? ")
+class quiz():
+    def __init__(self):
+        self.questions = {
+             "What is the full form of CPU?" : "Central Processing Unit",
+            "What is the full form of RAM?" : "Random Access Memory",
+            "What is the largest mammal on planet Earth?" : "Blue Whale",
+            "What is the scientific name of the most intelligent species on Earth?" : "Homo Sapiens"
+        }
+        self.score = 0
+        self.start_time = None
+        self.end_time = None
+    
+    def start(self):
+        print("welcome to quiz")
+        player_state = input("do u wanna play (Yes/no) ? ")
+        if player_state != "yes" :
+            print("maybe next time! begin")
+            return
+        print("let's begin")
+        self.start_time = time.time()
+        self.ask_questions()
+        self.end_time = time.time()
+        self.show_results()
 
-if player == "yes":
-    print("lets begin")
+    def ask_questions(self):
+        question_list = list(self.questions.items())
+        random.shuffle(question_list)
 
-score = 0 
-start_time = time.time()
-questions = {
-    "what is the fullform of cpu ?" : "Central Processing Unit",
-    "what is the fullform of ram ?" : "Random Acess Memory",
-    "what is the largest mammal of the planet earth?" : "Blue Whale",
-    "what is the scientific name of the most intelligent species in earth" : "Homo Sapiens"
-}
-for question,answer in random.shuffle(questions.items()):
-    print(question)
-    ans = input()
-    if ans.title() == answer:
-        print("correct ans!!")
-        score += 1
-    else:
-        print("wrong ans!")
+        for question,answer in question_list:
+            print(question)
+            ans = input("whats your answer ?")
+            if ans.strip().lower() == answer:
+                print("correct answer!!")
+                self.score += 1
+            else:
+                print("wrong answer! the correct answer is", answer)
 
-end_time = time.time()
+    def show_results(self):
+        print("score of the player is,", self.score)
+        elapsed_time = self.end_time - self.start_time
+        mins = int(elapsed_time//60)
+        secs = int(elapsed_time%60)
 
-elapsed_time = end_time - start_time
-
-mins = int(elapsed_time//60)
-secs = int(elapsed_time%60)
-
-print("the quiz has finally finished")
-
-print("you did " + str(score) + " no of questions correctly out of 4 questions")
-
-print("your percentage is", (score/4)*100)
-
-print("time taken to complete:", mins,"mins and ", secs, "seonds")
-
-print("thanks for playing")
+        print("quiz succesfully completed")
+        print("time taken to complete", mins,"mins", secs, "secs")
+        print("score obtained: ", self.score )
+    
+if __name__ == "__main__":
+    Quiz = quiz()
+    Quiz.start()
