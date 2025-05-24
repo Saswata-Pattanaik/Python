57c020799bbe384eada6a7625ac89b6cda0ca37b
Saswata Pattanaik <saswatap.ai09@gmail.com>
2025-05-24 18:41:54 +0530

    fixed minor bugs

--- roll_based_game.py
+++ roll_based_game.py
@@ -26,19 +26,21 @@ scores = []
 score = 0
 
 if __name__ == "__main__":
-    print("you can choose any no of rounds to play but if you get 1 on dice all your previous score would be reverted to 0")
+    print("you can choose any no of rounds to play but if you get 1 on dice all your previous score would be reverted to 0 and your turn is terminated")
     for i in range(0,np):
         print("player", i , "turn: ")
-        turns = int(input("please enter the no of turns you want to play: "))
-        for j in range(1,turns+1):
+        cond = int(input("please enter the no of turns you want to play: "))
+        for j in range(1,cond+1):
             num = roll_dice()
             print(j,")", 'you get', num)
             if num != 1:
                 score += num
             else:
                 score = 0
+                break
         scores.append(score)
         score = 0
+        
     
     print("game finished")
     print("final scores are as follows:")
